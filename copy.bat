e4eb04f1b79dc461cb4dcf931a2e169312f9ced9
qiangtou <qiangtou@gmail.com>
2013-08-27 11:40:44 +0800

    add copy cmd

---
+++ copy.bat
@@ -0,0 +1,23 @@
+@echo 请先关闭浏览器
+@echo 1.将数据文件备份至当前文件夹,方便u盘带走
+@echo 2.将数据文件恢复到浏览器存储空间中去
+@echo 其他键退出
+@echo off
+set /p a=请输入1或2按回车:
+if "%a%" == "1" goto copy
+if "%a%" == "2" goto backup 
+exit
+:copy
+@echo on
+copy "%localappdata%\Google\Chrome\User Data\Default\Local Storage\__0.localstorage" %cd%
+@echo off
+goto p
+
+:backup
+@echo on
+copy %cd%\__0.localstorage "%localappdata%\Google\Chrome\User Data\Default\Local Storage\" 
+@echo off
+goto p
+
+:p
+pause
